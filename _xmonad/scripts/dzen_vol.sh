3b7dfda4c5bfe3405994fd8396bc06fcc78d8490
Josh Proehl <josh@daedalusdreams.com>
2015-10-26 14:20:49 -0700

    Add mute display and toggle to the dzen vol menu

--- _xmonad/scripts/dzen_vol.sh
+++ _xmonad/scripts/dzen_vol.sh
@@ -2,18 +2,29 @@
 source $(dirname $0)/config.sh
 XPOS=$((1092 + $XOFFSET))
 WIDTH="15"
-LINES="13"
+LINES="14"
 
 vol=$(amixer get Master | egrep -o "[0-9]+%" | head -1 | egrep -o "[0-9]*")
 val=()
 
+ASTAT=`amixer get Master | awk 'END{gsub(/\[|\]|%/,""); print $6}'`
+BARCOLOR=""
+MUTECMD=""
+if [[ $ASTAT = "on" ]]; then
+  BARCOLOR=$white0
+  MUTECMD="mute"
+else
+  BARCOLOR="#ff0000"
+  MUTECMD="unmute"
+fi
+
 for i in {0..100..10}; do
 	if [ $vol -gt $i ]
 	then
-		val+=("^bg($white0)^ca(1,amixer set Master $i)      ^ca()")	
+		val+=("^bg($BARCOLOR)^ca(1,amixer set Master $i)      ^ca()")	
 	else
 		val+=("^bg($background)^ca(1,amixer set Master $i)      ^ca()")
 	fi
 done
 
-(echo "Volume"; echo " +"; echo "${val[10]}"; echo "${val[9]}"; echo "${val[8]}"; echo "${val[7]}"; echo "${val[6]}"; echo "${val[5]}"; echo "${val[4]}"; echo "${val[3]}"; echo "${val[2]}"; echo "${val[1]}"; echo "${val[0]}"; echo " -";sleep 15) | dzen2 -fg $foreground -bg $background -fn $FONT -x $XPOS -y $YPOS -w $WIDTH -l $LINES -e 'onstart=uncollapse,hide;button1=exit;button3=exit'
+(echo "Volume"; echo " +"; echo "${val[10]}"; echo "${val[9]}"; echo "${val[8]}"; echo "${val[7]}"; echo "${val[6]}"; echo "${val[5]}"; echo "${val[4]}"; echo "${val[3]}"; echo "${val[2]}"; echo "${val[1]}"; echo "${val[0]}"; echo " -"; echo " ^bg($background)^ca(1,amixer set Master $MUTECMD)M     ^ca()";sleep 15) | dzen2 -fg $foreground -bg $background -fn $FONT -x $XPOS -y $YPOS -w $WIDTH -l $LINES -e 'onstart=uncollapse,hide;button1=exit;button3=exit'
