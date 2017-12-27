102e7ee835b6d80afc199b2dac2b0537cd1e11a0
Josh Proehl <josh@daedalusdreams.com>
2017-12-27 10:40:17 -0800

    Move legacy screenlayout files to baldr- prefix.

---
+++ _screenlayout/baldr-vga-right-vert-flattop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1366x768 --rotate normal --output VGA1 --right-of LVDS1 --mode 1920x1080 --rotate right
---
+++ _screenlayout/baldr-vga-right-vert.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1366x768 --pos 0x832 --rotate normal --output VGA1 --mode 1920x1080 --pos 1366x0 --rotate right
+xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1366x768 --pos 0x1152 --rotate normal --output VGA1 --mode 1920x1080 --pos 1366x0 --rotate right
